d7c99f66943f910f3b2f9e0431e45427c4f96a0f
Steve Bendrick <steven.bendrick@gmail.com>
2021-09-04 23:17:34 -0400

    searching city appends header to current weather box

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var apiWeatherUrl = "https://api.openweathermap.org/data/2.5/onecall?";
 var apiKey = "&appid=aded44f82b44e0af228ba72e16261166";
 // var cityName = "New Milford,CT,US"
 var apiExclude = "&exclude=minutely,hourly"
+var apiUnits = "&units=imperial"
 
 
 var locationTextGet = function(event) {
@@ -30,29 +31,46 @@ var locationFetch = function(cityName) {
         console.log(response);
         var lat = response[0].lat;
         var lon = response[0].lon;
-        displayWeather(response[0].name);
-        weatherFetch(lat, lon);
+        var city = response[0].name;
+        weatherFetch(lat, lon, city);
       }
     });
   });
 }
 
-var weatherFetch = function(lat, lon) {
-  var weatherUrl = apiWeatherUrl + 'lat=' + lat + '&lon=' + lon + apiExclude + apiKey;
+var weatherFetch = function(lat, lon, city) {
+  var weatherUrl = apiWeatherUrl + 'lat=' + lat + '&lon=' + lon + apiUnits + apiExclude + apiKey;
   console.log(weatherUrl);
   fetch(weatherUrl).then(function(response) {
     response.json().then(function(response) {
       console.log(response);
+      var icon = response.current.weather[0].icon;
+      displayWeather(city, icon);
     })
   })
 }
 
-var displayWeather = function(city) {
+var displayWeather = function(city, icon) {
+  // select current weather box and remove contents
   var weatherBox = document.querySelector('.current-weather');
-  var weatherHeader = document.createElement('h3');
-  weatherHeader.innerText = city + " (" + dayjs().format('D/M/YYYY') + ")";
-  console.log(weatherHeader);
-  weatherBox.appendChild(weatherHeader);
+  weatherBox.innerHTML = "";
+
+  // create elements
+  var weatherDivEl = document.createElement('div');
+  var weatherHeaderEl = document.createElement('h3');
+  var iconDivEl = document.createElement('div');
+  var iconImgEl = document.createElement('img');
+
+  // set content and attributes of elements
+  weatherDivEl.className = "weather-header";
+  weatherHeaderEl.innerText = city + " (" + dayjs().format('D/M/YYYY') + ")";
+  iconImgEl.setAttribute('src', 'http://openweathermap.org/img/wn/' + icon + '@2x.png')
+
+  // append the elements
+  iconDivEl.appendChild(iconImgEl);
+  weatherDivEl.appendChild(weatherHeaderEl);
+  weatherDivEl.appendChild(iconDivEl);
+  weatherBox.appendChild(weatherDivEl);
 
 }
 
