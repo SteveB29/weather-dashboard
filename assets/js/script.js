2c93b03fdd69663cba08933422c1916eba78a7ae
Steve Bendrick <steven.bendrick@gmail.com>
2021-09-04 21:16:16 -0400

    fetch city lat and lon and weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,44 @@
 var searchForm = document.querySelector('#submit-button');
-var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=";
+
+var apiLocationUrl = "https://api.openweathermap.org/geo/1.0/direct?q=";
+var apiWeatherUrl = "https://api.openweathermap.org/data/2.5/onecall?";
 var apiKey = "&appid=aded44f82b44e0af228ba72e16261166";
 // var cityName = "New Milford,CT,US"
 var apiExclude = "&exclude=minutely,hourly"
 
-var locationFetch = function() {
-  var apiLookup = apiUrl + cityName + apiKey;
-  console.log(apiLookup);
-  fetch(apiLookup).then(function(response) {
+
+var locationTextGet = function(event) {
+  event.preventDefault();
+  var city = document.querySelector('#city-search');
+  if (city.value) {
+    locationFetch(city.value);
+    city.value = "";
+  } else {
+    alert("Please enter a city");
+  }
+}
+
+var locationFetch = function(cityName) {
+  var locationUrl = apiLocationUrl + cityName + apiKey;
+  fetch(locationUrl).then(function(response) {
     response.json().then(function(response) {
       console.log(response);
+      var lat = response[0].lat;
+      var lon = response[0].lon;
+      weatherFetch(lat, lon);
     });
   });
 }
 
-var locationGet = function(event) {
-  event.preventDefault();
-  var city = document.querySelector('#city-search');
-  console.log(city.value);
-  city.value = "";
+var weatherFetch = function(lat, lon) {
+  var weatherUrl = apiWeatherUrl + 'lat=' + lat + '&lon=' + lon + apiExclude + apiKey;
+  console.log(weatherUrl);
+  fetch(weatherUrl).then(function(response) {
+    response.json().then(function(response) {
+      console.log(response);
+    })
+  })
 }
 
-// locationFetch();
 
-searchForm.addEventListener('click', locationGet);
+searchForm.addEventListener('click', locationTextGet);
